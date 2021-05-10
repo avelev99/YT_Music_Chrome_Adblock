a43613140c5b6c63bf6e9c743e32cdc09ca93509
Adrian Velev <veleva@uni.coventry.ac.uk>
2021-05-10 14:56:18 +0300

    Added work on login module. Not yet entirely functional

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+from Utils import *
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -6,7 +8,6 @@ from selenium.webdriver.support.expected_conditions import presence_of_element_l
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.action_chains import ActionChains
 
-#This example requires Selenium WebDriver 3.13 or newer
 
 options = webdriver.ChromeOptions()
 options.binary_location = "C:/Program Files (x86)/chrome-win/chrome.exe"
@@ -14,6 +15,17 @@ chrome_driver_binary = "./Utils/chromedriver.exe"
 driver = webdriver.Chrome(chrome_driver_binary, chrome_options=options)
 wait = WebDriverWait(driver, 10)
 driver.get("https://music.youtube.com/")
-driver.find_element(By.NAME, "q").send_keys("cheese" + Keys.RETURN)
-first_result = wait.until(presence_of_element_located((By.CSS_SELECTOR, "h3>div")))
-print(first_result.get_attribute("textContent"))
+#driver.find_element(By.NAME, "SIGN IN").send_keys("cheese" + Keys.RETURN)
+#first_result = wait.until(presence_of_element_located((By.CSS_SELECTOR, "h3>div")))
+#print(first_result.get_attribute("textContent"))
+
+def Login():
+    sign_in = driver.find_element_by_xpath('//button[text()="SIGN IN"]')
+    sign_in.click
+    wait.until(ec.presence_of_element_located((By.ID, "identifierNext")))
+    text_input(getUsername())
+
+def text_input(text):
+    action = ActionChains(driver)
+    
+
