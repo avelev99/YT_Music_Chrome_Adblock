539796d8f5f1b9bf18d79e6d05f538f50391cb01
Adrian Velev <veleva@uni.coventry.ac.uk>
2021-05-12 16:07:17 +0300

    Deleted old segment of not properly working commented out code. Added text insertion action chain (line 21)

--- Main.py
+++ Main.py
@@ -16,36 +16,14 @@ wait = WebDriverWait(driver, 10)
 def login():
     login_btn = driver.find_element_by_css_selector(".sign-in-link.ytmusic-nav-bar")
     login_btn.click()
+    wait.until(EC.visibility_of_element_located((By.ID, 'identifierId')))
 
+def ins_text(text):
+    actions = ActionChains(driver)
+    actions.send_keys(text)
+    actions.perform()
+
+"""def next():
+    ActionChains(driver).send_keys(Keys.TAB * 2).perform()"""
 
 login()
-
-
-
-
-
-"""options = webdriver.ChromeOptions()
-options.binary_location = "C:/Program Files (x86)/chrome-win/chrome.exe"
-chrome_driver_binary = "./Utils/chromedriver.exe"
-driver = webdriver.Chrome(chrome_driver_binary, chrome_options=options)
-wait = WebDriverWait(driver, 10)
-driver.get("https://music.youtube.com/")
-#driver.find_element(By.NAME, "SIGN IN").send_keys("cheese" + Keys.RETURN)
-#first_result = wait.until(presence_of_element_located((By.CSS_SELECTOR, "h3>div")))
-#print(first_result.get_attribute("textContent"))
-
-def Login():
-    sign_in = driver.find_element_by_xpath("//button[text()='Sign in']")
-    sign_in.click
-    wait.until(EC.presence_of_element_located((By.ID, "identifierNext")))
-    text_input(getUsername())
-
-def text_input(text):
-    action = ActionChains(driver)
-    action.send_keys(text)
-    action.perform()
-
-def tabkey():
-    driver.find_element(By.NAME, "q").send_keys(Keys.TAB)
-
-Login()"""
