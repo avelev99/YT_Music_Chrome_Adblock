d52aca4731b28e1a9fc9eb21193b9318b9dbcab5
Adrian Velev <veleva@uni.coventry.ac.uk>
2021-05-12 15:01:41 +0300

    Fixed Sign In Button Not Clickable. Changed Xpath to CSS Selector

--- Main.py
+++ Main.py
@@ -1,16 +1,30 @@
-from winreg import HKEY_PERFORMANCE_DATA
-from Utils import *
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.expected_conditions import presence_of_element_located
-from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.action_chains import ActionChains
+from Utils import *
 
 
-options = webdriver.ChromeOptions()
+driver = webdriver.Chrome()
+driver.get("https://music.youtube.com/")
+
+wait = WebDriverWait(driver, 10)
+
+def login():
+    login_btn = driver.find_element_by_css_selector(".sign-in-link.ytmusic-nav-bar")
+    login_btn.click()
+
+
+login()
+
+
+
+
+
+"""options = webdriver.ChromeOptions()
 options.binary_location = "C:/Program Files (x86)/chrome-win/chrome.exe"
 chrome_driver_binary = "./Utils/chromedriver.exe"
 driver = webdriver.Chrome(chrome_driver_binary, chrome_options=options)
@@ -21,9 +35,9 @@ driver.get("https://music.youtube.com/")
 #print(first_result.get_attribute("textContent"))
 
 def Login():
-    sign_in = driver.find_element_by_xpath('//button[text()="SIGN IN"]')
+    sign_in = driver.find_element_by_xpath("//button[text()='Sign in']")
     sign_in.click
-    wait.until(ec.presence_of_element_located((By.ID, "identifierNext")))
+    wait.until(EC.presence_of_element_located((By.ID, "identifierNext")))
     text_input(getUsername())
 
 def text_input(text):
@@ -33,3 +47,5 @@ def text_input(text):
 
 def tabkey():
     driver.find_element(By.NAME, "q").send_keys(Keys.TAB)
+
+Login()"""
